418790b833f57a21402e48c05801bbff2c488d39
oiiemon9 <oiiemon9@gmail.com>
2025-08-27 13:25:18 +0600

    call button function and clear button function compliete

--- js/home.js
+++ js/home.js
@@ -1,1 +1,53 @@
-console.log('object');
+const callButtons = document.getElementsByClassName('call-btn');
+let callHistoryArr = [];
+
+function inerTextNum(inerTextId) {
+  const text = parseInt(document.getElementById(inerTextId).innerText);
+  return text;
+}
+// call button ok
+for (const callButton of callButtons) {
+  callButton.addEventListener('click', function () {
+    const coin = inerTextNum('service-coin');
+    if (coin < 20) {
+      alert(
+        "❌ You don't have enough coins! You need at least 20 coins to make a call."
+      );
+      return;
+    } else {
+      document.getElementById('call-history-section').innerText = '';
+      const cardTitle =
+        callButton.parentNode.parentNode.children[0].children[1].innerText;
+      const callNumber =
+        callButton.parentNode.parentNode.children[0].children[3].innerText;
+      alert(`📞 Calling ${cardTitle} ${callNumber}...`);
+      document.getElementById('service-coin').innerText = coin - 20;
+      const historyAdd = {
+        title: cardTitle,
+        number: callNumber,
+        time: new Date().toLocaleTimeString(),
+      };
+      callHistoryArr.push(historyAdd);
+      const historySection = document.getElementById('call-history-section');
+      for (const callHistory of callHistoryArr) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+       <div class="flex items-center gap-4 justify-between p-4 bg-gray-100 rounded-2xl">
+              <div>
+                <h4 class="text-lg font-bold">${callHistory.title}</h4>
+                <p class="text-sm text-gray-500">${callHistory.number}</p>
+              </div>
+              <p>${callHistory.time}</p>
+        </div>
+      `;
+        historySection.appendChild(div);
+      }
+    }
+  });
+}
+
+document.getElementById('clear-btn').addEventListener('click', function () {
+  const historySection = document.getElementById('call-history-section');
+  callHistoryArr = [];
+  historySection.innerText = '';
+});
